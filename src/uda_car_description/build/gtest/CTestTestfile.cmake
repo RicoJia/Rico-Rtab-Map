20635e52789980656333ccf002e547d37d8e8826
ricocoyeah <rjia@diligentrobots.com>
2023-12-29 12:02:46 -0600

    initial try

---
+++ src/uda_car_description/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/rjia/file_exchange_port/Udacity-Robotics-Software-Engineering/src/uda_car_description/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ src/uda_car_description/build/gtest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googlemock
+# Build directory: /home/rjia/file_exchange_port/Udacity-Robotics-Software-Engineering/src/uda_car_description/build/gtest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
